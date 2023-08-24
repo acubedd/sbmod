11cd0777f6dc4bc9ba6b84635361595bc705ab94
Alan Zheng <azheng106@gmail.com>
2023-08-24 12:33:37 -0400

    test

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -5,10 +5,11 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
+@Mod(modid = "sbhelper", name = "Skyblock Helper", version = "1.0")
 public class ExampleMod
 {
-    public static final String MODID = "examplemod";
+    public static final String MODID = "sbhelper";
+    public static final String NAME = "Skyblock Helper";
     public static final String VERSION = "1.0";
     
     @EventHandler
