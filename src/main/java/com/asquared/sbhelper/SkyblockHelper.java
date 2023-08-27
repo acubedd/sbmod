e18116bcabdab2643f567ac8c5f1c3fa6b30a79a
Alan Zheng <azheng106@gmail.com>
2023-08-27 13:52:28 -0400

    day 1 progress

---
+++ src/main/java/com/asquared/sbhelper/SkyblockHelper.java
@@ -0,0 +1,31 @@
+package com.asquared.sbhelper;
+
+import com.asquared.sbhelper.util.ChatUtils;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.init.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent;
+
+@Mod(modid = "sbhelper", name = "Skyblock Helper", version = "1.0", acceptedMinecraftVersions = "1.8.9")
+public class SkyblockHelper
+{
+    public static final String MODID = "sbhelper";
+    public static final String NAME = "Skyblock Helper";
+    public static final String VERSION = "1.0";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent e)
+    {
+		MinecraftForge.EVENT_BUS.register(this);
+        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+    }
+
+    @SubscribeEvent
+    public void onLogin(PlayerEvent.PlayerLoggedInEvent e) {
+        ChatUtils.sendMessageToPlayer((EntityPlayerMP) e.player, "Welcome");
+    }
+}
