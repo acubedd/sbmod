3c77bbed4fa33aab13e5676cb66c3efabe59c28d
Albert Chen <chenalbert06@gmail.com>
2023-08-28 16:46:00 -0400

    add proxy

--- src/main/java/io/github/acubed/SkyblockHelper/Reference.java
+++ src/main/java/io/github/acubed/SkyblockHelper/Reference.java
@@ -5,4 +5,7 @@ public class Reference {
     public static final String NAME = "Skyblock Helper";
     public static final String VERSION = "1.0";
     public static final String ACCEPTED_VERSIONS = "1.8.9";
+
+    public static final String CLIENT_PROXY_CLASS = "io.github.acubed.SkyblockHelper.proxy.ClientProxy";
+    public static final String SERVER_PROXY_CLASS = "io.github.acubed.SkyblockHelper.proxy.CommonProxy";
 }
--- src/main/java/io/github/acubed/SkyblockHelper/SkyblockHelper.java
+++ src/main/java/io/github/acubed/SkyblockHelper/SkyblockHelper.java
@@ -1,12 +1,15 @@
 package io.github.acubed.SkyblockHelper;
 
 import io.github.acubed.SkyblockHelper.events.loginHandler;
+import io.github.acubed.SkyblockHelper.proxy.ClientProxy;
 import io.github.acubed.SkyblockHelper.util.ChatUtils;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.init.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent;
@@ -14,9 +17,25 @@ import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 @Mod(modid = Reference.MODID, name = Reference.NAME, version = Reference.VERSION, acceptedMinecraftVersions = Reference.ACCEPTED_VERSIONS)
 public class SkyblockHelper
 {    
+    @SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS)
+    public static ClientProxy proxy;
+
+    @Instance("SkyblockHelper")
+    public static SkyblockHelper instance;
+
+    @EventHandler
+    public void preInit(FMLInitializationEvent e)
+    {
+    }
+
     @EventHandler
     public void init(FMLInitializationEvent e)
     {
 		MinecraftForge.EVENT_BUS.register(new loginHandler());
+    }
+
+    @EventHandler
+    public void postInit(FMLInitializationEvent e)
+    {
     }
 }
