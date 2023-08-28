93b795582a6111b79d451a5c58f45fdabd85ad00
Albert Chen <chenalbert06@gmail.com>
2023-08-28 16:46:35 -0400

    fixed login to be client side but for some reason crashes when launched in singleplayer

--- src/main/java/io/github/acubed/SkyblockHelper/events/loginHandler.java
+++ src/main/java/io/github/acubed/SkyblockHelper/events/loginHandler.java
@@ -1,13 +1,19 @@
 package io.github.acubed.SkyblockHelper.events;
 
+import io.github.acubed.SkyblockHelper.proxy.ClientProxy;
 import io.github.acubed.SkyblockHelper.util.ChatUtils;
-import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraftforge.client.event.ClientChatReceivedEvent;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 
 public class loginHandler {
     @SubscribeEvent
-    public void onLogin(PlayerEvent.PlayerLoggedInEvent e) {
-        ChatUtils.sendMessageToPlayer((EntityPlayerMP) e.player, "commands and info for mod to be added");
+    public void onLogin(EntityJoinWorldEvent e) {
+        if (e.entity != null && e.entity instanceof EntityPlayer) {
+            ChatUtils.sendMessageToPlayer("commands and info for mod to be added");
+        }
     }
 }
