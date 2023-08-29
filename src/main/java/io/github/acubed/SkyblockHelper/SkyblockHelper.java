1512d6d76309dfd6f55f476033e901dc635a7741
Albert Chen <chenalbert06@gmail.com>
2023-08-29 15:01:08 -0400

    update login send (no longer crashes)

--- src/main/java/io/github/acubed/SkyblockHelper/SkyblockHelper.java
+++ src/main/java/io/github/acubed/SkyblockHelper/SkyblockHelper.java
@@ -2,17 +2,12 @@ package io.github.acubed.SkyblockHelper;
 
 import io.github.acubed.SkyblockHelper.events.loginHandler;
 import io.github.acubed.SkyblockHelper.proxy.ClientProxy;
-import io.github.acubed.SkyblockHelper.util.ChatUtils;
-import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.init.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 
 @Mod(modid = Reference.MODID, name = Reference.NAME, version = Reference.VERSION, acceptedMinecraftVersions = Reference.ACCEPTED_VERSIONS)
 public class SkyblockHelper
--- src/main/java/io/github/acubed/SkyblockHelper/events/loginHandler.java
+++ src/main/java/io/github/acubed/SkyblockHelper/events/loginHandler.java
@@ -1,18 +1,36 @@
 package io.github.acubed.SkyblockHelper.events;
 
-import io.github.acubed.SkyblockHelper.proxy.ClientProxy;
 import io.github.acubed.SkyblockHelper.util.ChatUtils;
-import net.minecraft.client.Minecraft;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraftforge.client.event.ClientChatReceivedEvent;
-import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.PlayerEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent.ClientTickEvent;
+import net.minecraftforge.fml.common.network.FMLNetworkEvent.ClientConnectedToServerEvent;
 
 public class loginHandler {
+    private boolean worldLoaded = false;
+    private boolean clientConnected = false;
+
     @SubscribeEvent
-    public void onLogin(EntityJoinWorldEvent e) {
-        if (e.entity != null && e.entity instanceof EntityPlayer) {
+    public void onLogin(ClientConnectedToServerEvent e) {
+        this.clientConnected = true;
+
+        System.out.println("*********Connected to server*********");
+    }
+
+    @SubscribeEvent
+    public void onWorldLoad(WorldEvent.Load e) {
+        this.worldLoaded = true;
+
+        System.out.println("*********World loaded*********");
+    }
+
+    @SubscribeEvent
+    public void onTick(ClientTickEvent e) {
+        if (this.worldLoaded && this.clientConnected) {
+            this.worldLoaded = false;
+            this.clientConnected = false;
+            System.out.println("**************************************CONNECTED**************************************");
+
             ChatUtils.sendMessageToPlayer("commands and info for mod to be added");
         }
     }
