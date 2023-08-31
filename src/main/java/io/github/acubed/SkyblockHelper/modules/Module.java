b7c8a9d61f2fdad555928c3ef47f22e396d6a96d
Alan Zheng <azheng106@gmail.com>
2023-08-31 18:31:09 -0400

    change Module class

--- src/main/java/io/github/acubed/SkyblockHelper/modules/Module.java
+++ src/main/java/io/github/acubed/SkyblockHelper/modules/Module.java
@@ -1,34 +1,27 @@
 package io.github.acubed.SkyblockHelper.modules;
 
 public class Module {
-    private String name;
-    private boolean toggled;
-
-    public Module(String name) {
-        this.name = name;
-    }
+    private boolean toggled = false;
 
     public boolean isToggled() {
         return this.toggled;
-    }
-
-    public String getName() {
-        return this.name;
     }
 
     public void toggle() {
         this.toggled = !this.toggled;
 
         if (this.toggled) {
-            this.onEnable();
+            onEnable();
         } else {
-            this.onDisable();
+            onDisable();
         }
     }
 
-    public void onEnable() {
-    }
+    public void onEnable() {}
 
-    public void onDisable() {
-    }
+    public void onDisable() {}
+
+    public void onTick() {}
+
+    public void onChatMessageReceived(String msg) {}
 }
